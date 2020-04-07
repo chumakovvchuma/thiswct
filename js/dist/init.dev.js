a7f2bc1e1097c6a35a8e16ba82b5bb9044cb19cf
Victor Chumakov <victorchumakov@MacBook-Air-Victor.local>
2020-04-08 02:39:31 +0300

    paste

--- js/dist/init.dev.js
+++ js/dist/init.dev.js
@@ -1,61 +1,51 @@
-(function($){
-  $(function(){
+"use strict";
+
+(function ($) {
+  $(function () {
     $('.carousel').carousel({
       numVisible: 9,
       padding: 200
     });
     $('.modal').modal();
-
-
-    
-
     $.scrollify({
-      section : ".scrollLink",
-      sectionName : "section-name",
-      interstitialSection : "",
+      section: ".scrollLink",
+      sectionName: "section-name",
+      interstitialSection: "",
       easing: "easeOutExpo",
       scrollSpeed: 1800,
-      offset : 0,
+      offset: 0,
       scrollbars: false,
       standardScrollElements: "",
       setHeights: true,
       overflowScroll: true,
       updateHash: true,
-      touchScroll:true,
-      before:function(i,panels) {
-
+      touchScroll: true,
+      before: function before(i, panels) {
         var ref = panels[i].attr("data-section-name");
-  
         $(".pagination .active").removeClass("active");
-  
         $(".pagination").find("a[href=\"#" + ref + "\"]").addClass("active");
-      },       
-      after:function() {},
-      afterResize:function() {},
-      afterRender:function() {
+      },
+      after: function after() {},
+      afterResize: function afterResize() {},
+      afterRender: function afterRender() {
         var pagination = "<ul class=\"pagination\">";
         var activeClass = "";
-        $(".scrollLink").each(function(i) {
+        $(".scrollLink").each(function (i) {
           activeClass = "";
-          if(i===$.scrollify.currentIndex()) {
+
+          if (i === $.scrollify.currentIndex()) {
             activeClass = "active";
           }
+
           pagination += "<li><a class=\"" + activeClass + "\" href=\"#" + $(this).attr("data-section-name") + "\"><span class=\"hover-text\">" + $(this).attr("data-section-name").charAt(0).toUpperCase() + $(this).attr("data-section-name").slice(1) + "</span></a></li>";
         });
-  
         pagination += "</ul>";
-  
         $(".menu").append(pagination);
-        $(".pagination a").on("click",$.scrollify.move);
+        $(".pagination a").on("click", $.scrollify.move);
       }
     });
-    
-      
-    
   }); // end of document ready
 })(jQuery); // end of jQuery name space
-
-
 // $( ".menu-one" ).slideToggle("slow");
 // $( ".menu-two" ).slideToggle("slow");
 // $( ".menu-three" ).slideToggle("slow");
